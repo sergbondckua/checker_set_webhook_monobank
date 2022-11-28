8fae82ab57684c5f8a92fa41182d4cbd9ffb4337
sergbondckua <sergninja@gmail.com>
2022-11-28 02:17:23 +0200

    start first code

--- main.py
+++ main.py
@@ -25,9 +25,11 @@ class SetMonoWebhook(Client):
         """
         try:
             if self.get_client_info().get('webHookUrl'):
+                logging.info("Webhook alive")
                 return True
-        except monobank.Error as error:
+        except monobank.errors.Error as error:
             raise monobank.errors.Error("Unable to retrieve data", error)
+        logging.warning("Not found webhook")
         return False
 
     def install_webhook(self, url: str) -> dict:
@@ -43,9 +45,8 @@ class SetMonoWebhook(Client):
                 logging.info("Webhook was created")
                 return {"status": "created"}
             except monobank.errors.Error as error:
-                raise monobank.Error("Unable to create webhook", error)
-        logging.info("Webhook alive")
-        return {"status": "success"}
+                raise monobank.errors.Error("Unable to create webhook", error)
+        return {"status": "alive"}
 
 
 def main():
@@ -55,12 +56,11 @@ def main():
 
 
 if __name__ == '__main__':
-    # main()
     # Launch Scheduler
     scheduler = BlockingScheduler(timezone=pytz.timezone("Europe/Kiev"))
 
-    # Каждые понедельник в 12:00
-    scheduler.add_job(main, 'cron', day_of_week='mon', hour='12', minute="00")
+    # Specified time
+    scheduler.add_job(main, 'cron', day_of_week='*', hour='*/1', minute="*")
 
     # Starting the daemon
     scheduler.start()
