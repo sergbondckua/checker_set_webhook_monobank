5b8aa49d8fa5b22a5bd688476eacaa8901f4cbba
sergbondckua <sergninja@gmail.com>
2022-12-02 14:04:51 +0200

    update and fix

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ def main():
         mono_client = monobank.Client(token=os.environ.get("MONOBANK_TOKEN"))
     except monobank.TooManyRequests as error:
         logging.error(error)
-        time.sleep(5)
+        time.sleep(10)
         mono_client = monobank.Client(token=os.environ.get("MONOBANK_TOKEN"))
+        if not mono_client.get_client_info().get("webHookUrl"):
+            mono_client.create_webhook(url=os.environ.get("MONOBANK_WEBHOOK_URL"))
+            logging.info("Webhook was created")
+        else:
+            logging.info("Webhook is already installed")
     except monobank.Error as error:
-        raise monobank.Error("Wrong api token", error)
-    if not mono_client.get_client_info().get("webHookUrl"):
-        mono_client.create_webhook(url=os.environ.get("MONOBANK_WEBHOOK_URL"))
-        logging.info("Webhook was created")
-    else:
-        logging.info("Webhook is already installed")
+        logging.error(monobank.Error("Wrong api token", error))
 
 
 if __name__ == '__main__':
